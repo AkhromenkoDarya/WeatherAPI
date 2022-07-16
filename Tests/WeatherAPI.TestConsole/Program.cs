bb35424b3df31e01a05067a4bf3cef4b65bc8f9b
Darya Akhromenko <otulissa2012@yandex.ru>
2022-07-16 17:51:57 +0700

    Настройка тестового консольного проекта

--- Tests/WeatherAPI.TestConsole/Program.cs
+++ Tests/WeatherAPI.TestConsole/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace WeatherAPI.TestConsole
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static readonly IHost _hosting;
+
+        public static IHost Hosting = _hosting ??= CreateHostBuilder(
+            Environment.GetCommandLineArgs()).Build();
+
+        private static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(ConfigureServices);
+
+        public static IServiceProvider Services = Hosting.Services;
+
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
         {
-            Console.WriteLine("Hello World!");
+            services.AddHttpClient<WeatherApiClient>(client => client.BaseAddress = new Uri(
+                host.Configuration["WeatherApi"]));
+        }
+
+        static async Task Main(string[] args)
+        {
+            using IHost host = Hosting;
+            await host.StartAsync();
+
+            var weather = host.Services.GetRequiredService<WeatherApiClient>();
+
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+
+            await host.StopAsync();
         }
     }
 }
