6267bb703239e69b06f3c2eb0a2ca2de48766d4f
Darya Akhromenko <otulissa2012@yandex.ru>
2022-07-18 08:32:46 +0700

    Расширение перечисления WeatherApiType (получаем часть пути к ресурсу, содержающую используемый API и формат возвращаемых данных)

---
+++ WeatherAPI/Common/Enums/WeatherApiTypeExtension.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherAPI.Common.Enums
+{
+    internal static class WeatherApiTypeExtension
+    {
+        internal static string GetResourcePath(this WeatherApiType apiType) => "/v1/" + 
+            apiType.ToString().ToLower() + ".json";
+    }
+}
